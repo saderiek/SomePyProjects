5e7b665e944b8725836414083ecf126b3806c209
Vaselissa <vasilisapulvas@yandex.ru>
2023-02-16 23:46:50 +0300

    FInally

--- lab1.py
+++ lab1.py
@@ -3,29 +3,33 @@ import argparse
 import shutil
 import winreg
 
-def create_file(fname:str) -> None:
+
+def create_file(fname: str) -> None:
     try:
         with open(fname, "x") as fd:
             pass
     except FileExistsError:
         print("File already exists")
 
-def delete_file(fname:str) -> None:
+
+def delete_file(fname: str) -> None:
     try:
-        os.remove(fname)   
+        os.remove(fname)
     except FileNotFoundError:
         print("No such file")
 
-def write_file(fname:str, text:str) -> None:
+
+def write_file(fname: str, text: str) -> None:
     try:
-        with open (fname, "w") as fd:
+        with open(fname, "w") as fd:
             fd.write(text)
     except FileNotFoundError:
         print("Error")
     except PermissionError:
         print("Permission denied")
 
-def read_file(fname:str) -> None:
+
+def read_file(fname: str) -> None:
     try:
         with open(fname, "r") as fd:
             print("File contents:")
@@ -33,19 +37,22 @@ def read_file(fname:str) -> None:
     except FileNotFoundError:
         print("No such file")
 
-def copy_file(source:str, destination:str) -> None:
+
+def copy_file(source: str, destination: str) -> None:
     try:
         shutil.copyfile(source, destination)
     except FileNotFoundError:
         print("No such file")
 
-def rename_file(fname:str, new_name:str) -> None:
+
+def rename_file(fname: str, new_name: str) -> None:
     try:
         os.rename(fname, new_name)
     except FileNotFoundError:
         print("No such file")
 
-def create_key(key:str) -> None:
+
+def create_key(key: str) -> None:
     try:
         path = 'Software\\'
         path += key
@@ -54,46 +61,71 @@ def create_key(key:str) -> None:
     except PermissionError:
         print("Permission denied")
 
-def delete_key(key:str) -> None:
+
+def delete_key(key: str) -> None:
     try:
-        pass
+        path = 'Software\\'
+        path += key
+        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        winreg.DeleteKey(reg, path)
     except PermissionError:
         print("Permission denied")
 
-def value_key(key:str) -> None:
+
+def value_key(key: str, name: str, value: str) -> None:
     try:
-        pass
+        path = 'Software\\'
+        path += key
+        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        open_key = winreg.OpenKeyEx(reg, path, 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(open_key, name, 0, winreg.REG_SZ, value)
+        winreg.CloseKey(reg)
     except PermissionError:
         print("Permission denied")
+
 
 def main():
     ## FILE SYSTEM ##
     parser = argparse.ArgumentParser(description='FS/Registry management')
     parser.add_argument('-cr', '--create', type=str, help='Creating a file')
     parser.add_argument('-d', '--delete', type=str, help='Deleting a file')
-    parser.add_argument('-w', '--write', nargs=2, type=str, help='Writing to a file')
+    parser.add_argument('-w', '--write', nargs=2,
+                        type=str, help='Writing to a file')
     parser.add_argument('-r', '--read', type=str, help='Reading from a file')
-    parser.add_argument('-rn', '--rename', nargs=2, type=str, help='Renaming a file')
-    parser.add_argument('-c', '--copy', nargs=2, type=str, help='Copying a file from one directory to another')
+    parser.add_argument('-rn', '--rename', nargs=2,
+                        type=str, help='Renaming a file')
+    parser.add_argument('-c', '--copy', nargs=2, type=str,
+                        help='Copying a file from one directory to another')
 
     ## REGISTRY ##
     parser.add_argument('-kc', '--kcreate', type=str, help='Creating a key')
     parser.add_argument('-kd', '--kdelete', type=str, help='Deleting a key')
-    parser.add_argument('-kv', '--kvalue', type=str, help='Writing a value to a keyy')
+    parser.add_argument('-kv', '--kvalue', nargs=3, type=str,
+                        help='Writing a value to a keyy')
 
     args = parser.parse_args()
 
-    if (args.create): create_file(args.create)
-    elif (args.delete): delete_file(args.delete)
-    elif (args.write): write_file(args.write[0], args.write[1])
-    elif (args.read): read_file(args.read)
-    elif (args.rename): rename_file(args.rename[0], args.rename[1])
-    elif (args.copy): copy_file(args.copy[0], args.copy[1])
+    if (args.create):
+        create_file(args.create)
+    elif (args.delete):
+        delete_file(args.delete)
+    elif (args.write):
+        write_file(args.write[0], args.write[1])
+    elif (args.read):
+        read_file(args.read)
+    elif (args.rename):
+        rename_file(args.rename[0], args.rename[1])
+    elif (args.copy):
+        copy_file(args.copy[0], args.copy[1])
+    elif (args.kcreate):
+        create_key(args.kcreate)
+    elif (args.kdelete):
+        delete_key(args.kdelete)
+    elif (args.kvalue):
+        value_key(args.kvalue[0], args.kvalue[1], args.kvalue[2])
+    else:
+        print("Something's gone wrong")
 
-    elif (args.kcreate): create_key(args.kcreate)
-    elif (args.kdelete): delete_key(args.kdelete)
-    elif (args.kvalue): value_key(args.kvalue)
-    else: print("Something's gone wrong")
 
 if __name__ == "__main__":
     main()
