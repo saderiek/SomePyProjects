822332b6c9db038a3144cc8d601a7e8811711f0e
Vaselissa <vasilisapulvas@yandex.ru>
2023-02-20 23:39:31 +0300

    Add work with HKLM, HKCC and others

--- lab1.py
+++ lab1.py
@@ -52,32 +52,53 @@ def rename_file(fname: str, new_name: str) -> None:
         print("No such file")
 
 
-def create_key(key: str) -> None:
+def create_key(cnst: str, key: str) -> None:
     try:
-        path = 'Software\\'
-        path += key
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        winreg.CreateKey(reg, path)
+        if cnst == 'HKCU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        elif cnst == 'HKCR':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT)
+        elif cnst == 'HKLM':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        elif cnst == 'HKU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_USERS)
+        elif cnst == 'HKCC':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_CONFIG)
+        winreg.CreateKey(reg, key)
     except PermissionError:
         print("Permission denied")
 
 
-def delete_key(key: str) -> None:
+def delete_key(cnst: str, key: str) -> None:
     try:
-        path = 'Software\\'
-        path += key
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        winreg.DeleteKey(reg, path)
+        if cnst == 'HKCU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        elif cnst == 'HKCR':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT)
+        elif cnst == 'HKLM':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        elif cnst == 'HKU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_USERS)
+        elif cnst == 'HKCC':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_CONFIG)
+        winreg.DeleteKey(reg, key)
     except PermissionError:
         print("Permission denied")
 
 
-def value_key(key: str, name: str, value: str) -> None:
+def value_key(cnst: str, key: str, name: str, value: str) -> None:
     try:
-        path = 'Software\\'
-        path += key
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
-        open_key = winreg.OpenKeyEx(reg, path, 0, winreg.KEY_WRITE)
+        if cnst == 'HKCU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        elif cnst == 'HKCR':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT)
+        elif cnst == 'HKLM':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        elif cnst == 'HKU':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_USERS)
+        elif cnst == 'HKCC':
+            reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_CONFIG)
+        open_key = winreg.OpenKeyEx(reg, key, 0, winreg.KEY_WRITE)
         winreg.SetValueEx(open_key, name, 0, winreg.REG_SZ, value)
         winreg.CloseKey(reg)
     except PermissionError:
@@ -85,7 +106,7 @@ def value_key(key: str, name: str, value: str) -> None:
 
 
 def main():
-    ## FILE SYSTEM ##
+    # FILE SYSTEM #
     parser = argparse.ArgumentParser(description='FS/Registry management')
     parser.add_argument('-cr', '--create', type=str, help='Creating a file')
     parser.add_argument('-d', '--delete', type=str, help='Deleting a file')
@@ -97,10 +118,12 @@ def main():
     parser.add_argument('-c', '--copy', nargs=2, type=str,
                         help='Copying a file from one directory to another')
 
-    ## REGISTRY ##
-    parser.add_argument('-kc', '--kcreate', type=str, help='Creating a key')
-    parser.add_argument('-kd', '--kdelete', type=str, help='Deleting a key')
-    parser.add_argument('-kv', '--kvalue', nargs=3, type=str,
+    # REGISTRY #
+    parser.add_argument('-kc', '--kcreate', nargs=2,
+                        type=str, help='Creating a key')
+    parser.add_argument('-kd', '--kdelete', nargs=2,
+                        type=str, help='Deleting a key')
+    parser.add_argument('-kv', '--kvalue', nargs=4, type=str,
                         help='Writing a value to a keyy')
 
     args = parser.parse_args()
@@ -118,11 +141,12 @@ def main():
     elif (args.copy):
         copy_file(args.copy[0], args.copy[1])
     elif (args.kcreate):
-        create_key(args.kcreate)
+        create_key(args.kcreate[0], args.kcreate[1])
     elif (args.kdelete):
-        delete_key(args.kdelete)
+        delete_key(args.kdelete[0], args.kdelete[1])
     elif (args.kvalue):
-        value_key(args.kvalue[0], args.kvalue[1], args.kvalue[2])
+        value_key(args.kvalue[0], args.kvalue[1],
+                  args.kvalue[2], args.kvalue[3])
     else:
         print("Something's gone wrong")
 
